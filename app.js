194b645af07c7c243b06e1fc47fd461fce0882ce
Edusky-Dev <eduardo.vilca@tecsup.edu.pe>
2026-04-16 16:59:59 -0500

    Subiendo proyecto Web Teran Veterinaria

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const indexRouter = require('./routes/index');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+
+// Configurar el motor de plantillas EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
+// Middleware para analizar los cuerpos de las solicitudes
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Rutas
+app.use('/', indexRouter);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor ejecutándose en http://localhost:${port}`);
+});
